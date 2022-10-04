336e7544379ae809541e4ffb81e17f08963011e6
IronBlue01 <danilo18programador@gmail.com>
2022-10-04 01:12:57 -0300

    add migration mesa

---
+++ database/migrations/2022_10_04_041033_create_mesa_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMesaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mesa', function (Blueprint $table) {
+            $table->id('id_mesa');
+            $table->integer('id_estabelecimento');
+            $table->integer('numero_mesa');
+            $table->text('qrcode_mesa');
+            $table->string('descricao_mesa');
+            $table->boolean('status_mesa');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mesa');
+    }
+}
