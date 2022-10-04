30e6bebf75cb29ca1552cbd75471928601761934
IronBlue01 <danilo18programador@gmail.com>
2022-10-04 00:47:56 -0300

    add migration cargo

---
+++ database/migrations/2022_10_04_034539_create_cargo_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCargoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cargo', function (Blueprint $table) {
+            $table->id('id_cargo');
+            $table->integer('id_estabelecimento');
+            $table->string('cargo_descricao');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cargo');
+    }
+}
